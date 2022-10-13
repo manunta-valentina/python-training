b8ebb35d33c77e627126e89182bc0f75e7b44bd1
manunta-valentina <manunta.valentina.gioia@iisgalvanimi.edu.it>
2022-10-13 10:47:18 +0000

    versione 0.1

---
+++ main_1.py
@@ -0,0 +1,9 @@
+x = 50    #dichiaro una variabile x, che è un numero intero
+y = "Aurora" #dichiaro una variabile y, che è un nome e va scritto dentro le virgolette
+z= 4.5      #dichiaro una variabile z, che è un numero decimale
+print(x)  #stampo la variable x, e mi comparirà il numero
+print(y) #stampo la variable y, e mi comparirà il nome
+print(z) #stampo la variable z, e mi comparirà il numero 
+print(type(x)) #stampo la variable x, e mi comparirà il tipo di variabile
+print(type(y)) #stampo la variable y, e mi comparirà il tipo di variabile
+print(type(z)) #stampo la variable z, e mi comparirà il tipo di variabile
