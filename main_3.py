d457a5c8fc4508aac12ac2ee55e12b8970de168d
manunta-valentina <manunta.valentina.gioia@iisgalvanimi.edu.it>
2022-10-20 10:37:36 +0000

    versione 0.1

---
+++ main_3.py
@@ -0,0 +1,8 @@
+a = 200 # #dichiaro una variabile a
+b = 33 # #dichiaro una variabile b 
+if b > a: #  utilizzo if per verificare se b è maggiore di a 
+  print("b è maggiore di a")
+elif a == b:
+  print("a e b sono uguali")
+else:
+  print("a è maggiore di b")
