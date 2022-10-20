91aece278b07e63a613281b971965dbae5f13514
manunta-valentina <manunta.valentina.gioia@iisgalvanimi.edu.it>
2022-10-20 10:26:07 +0000

    versione 0.1

--- main_2.py
+++ main_2.py
@@ -1,8 +1,5 @@
-thislist = ["apple", "banana", "cherry", "apple", "cherry"]
-print(thislist)
-print(len(thislist))
-print(type(thislist))
-
-list1 = ["abc", 34, True, 40, "male"]
-print(list1)
-
+thislist = ["apple", "banana", "cherry", "apple", "cherry", "abc", 34, True, 40, "male"]  #crea una lista, elenchi possono avere elementi 
+# con lo stesso valore, con stringhe, numeri interi e valori booleani
+print(thislist)  # stampo la lista
+print(len(thislist)) #stampo il numero di elementi nell'elenco
+print(type(thislist)) # stampo il tipo  il tipo di dati di un elenco 
